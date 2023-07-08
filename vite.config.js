deea6467e4267e3704f80383ed4a1b2c51552932
Refaster24 <refaster24@gmail.com>
2023-07-08 03:26:39 +0300

    vite file edit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import { resolve } from 'path'
+
+
+const outDir = resolve(__dirname,'dist')
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir,
+    emptyOutDir:true,
+    rollupOptions:{
+      input:{
+        index : resolve(__dirname,'index.html'),
+        about : resolve(__dirname,'about.html'),
+        // contact : resolve(__dirname,'contact.html'),
+      }
+    }
+  }
 })
